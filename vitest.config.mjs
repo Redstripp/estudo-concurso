d3ea677679720dfeba038c27a941c449e2ec577c
Redstripp <147215196+Redstripp@users.noreply.github.com>
2026-05-13 21:57:32 -0300

    Update vitest.config.mjs

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    setupFiles: ['./__tests__/setup.js']
+    include: ['tests/**/*.test.js'],
+    setupFiles: ['./tests/setup.js']
   }
 })
