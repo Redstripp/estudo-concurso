974fea557bd06b62d4928ea49ced02bd90249c54
Redstripp <147215196+Redstripp@users.noreply.github.com>
2026-05-15 10:44:48 -0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// js/config.js
+const SUPABASE_URL = 'https://hrwsddiggaumzjxqpyoj.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhyd3NkZGlnZ2F1bXpqeHFweW9qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NTUwODMsImV4cCI6MjA5MzQzMTA4M30.ofQ54DHsX5P8nag0oVqpNh3Lvnjj2wDU3TfAH_t2Oeg'
+
+window.ADMIN_EMAILS = ['ewertonc29@gmail.com']
+
+const { createClient } = supabase
+const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+window.db = db
