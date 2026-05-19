cc93403d2fb3eab5caed392a80366191081d8865
Redstripp <147215196+Redstripp@users.noreply.github.com>
2026-05-18 21:34:19 -0300

    Fase 2 continuacao utils (#83)
    * refactor: add TypeScript HTML escape utility (#81)
    
    * refactor: add TypeScript weak-registration alert utility (#82)

---
+++ src/utils/html.ts
@@ -0,0 +1,8 @@
+export function escaparHtmlSeguro(valor: unknown): string {
+  return String(valor ?? '')
+    .split('&').join('&amp;')
+    .split('<').join('&lt;')
+    .split('>').join('&gt;')
+    .split('"').join('&quot;')
+    .split("'").join('&#039;');
+}
